7fafd41f4186f73dc0868681ea6d256cb788ab08
wallacesfos <wallace.oliveira@aluno.faculdadeimpacta.com.br>
2021-11-11 10:14:40 -0300

    Fix: Add added the login style

---
+++ src/Pages/Login/style.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+import AlphaGif from '../../Assets/Alpha.gif'
+
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+`
+
+export const ImageDiv = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-image: url(${AlphaGif});
+    background-size: cover;
+    background-position: center;
+
+    
+    @media(max-width: 768px){
+        display: none;
+    }
+`
+
+export const Image = styled.img`
+    height: 254px;
+    margin-bottom: 20px;
+`
+
+export const SigninDiv = styled.div`
+    width: 500px;
+    height: 100vh;
+    background-color: #222A39;
+    display: flex;
+    flex-direction: column;
+    align-items: center;    
+    @media(max-width: 768px){
+        width: 100%;
+    }
+`
+
+export const RegisterForm = styled.div`
+    margin: 50px 0 2px 0;
+    display: flex;
+    flex-direction: column;
+    height: 330px;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+`
+
+export const Paragrafo = styled.p`
+    margin-top: 20px;
+    color: #19A19C;
+    cursor: pointer;
+`
